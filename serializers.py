4f58118fb7385e967339f34b7406b6087a0110ff
jupi2142 <jupi2142@users.noreply.github.com>
2017-02-02 09:26:18 +0300

    just incase map is a generator, like in python3

--- serializers.py
+++ serializers.py
@@ -14,7 +14,7 @@ class DynamicFieldsSerializerMixin(object):
         super(self.__class__, self).__init__(*args, **kwargs)
         current_fields = self.fields.keys()
         fields_to_remove = exclude or fields.symmetric_difference(current_fields)
-        _ = map(self.fields.pop, fields_to_remove)
+        list(map(self.fields.pop, fields_to_remove))
 
         
 class DynamicFieldsModelSerializer(DynamicFieldsSerializerMixin,
